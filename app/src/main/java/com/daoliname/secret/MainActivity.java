14a5004e71efc5fa58dc860434dc83e332daff62
MinghuiGao <ham_777@126.com>
2019-03-28 23:49:03 +0800

    save the qrcode bitmap to system album

--- app/src/main/java/com/daoliname/secret/MainActivity.java
+++ app/src/main/java/com/daoliname/secret/MainActivity.java
@@ -1,14 +1,21 @@
 package com.daoliname.secret;
 
 import android.graphics.Bitmap;
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.daoliname.secret.utils.QRCodeUtil;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -27,10 +34,23 @@ public class MainActivity extends AppCompatActivity {
         Bitmap qrCodeBitmap = QRCodeUtil.createQRCodeBitmap(encryptInfo, 100, 100);
         ((ImageView)findViewById(R.id.iv_qr_code)).setImageBitmap(qrCodeBitmap);
 
-        // TODO: 2019/3/28 0028 save bitmap image to sdcard.
-
-        
+        String path = MediaStore.Images.Media.insertImage(getContentResolver(), qrCodeBitmap, "", "");
+        Log.d(TAG,"path = " + path);
+        msc.connect();
     }
+
+    final MediaScannerConnection msc = new MediaScannerConnection(MainActivity.this, new MediaScannerConnection.MediaScannerConnectionClient() {
+
+        public void onMediaScannerConnected() {
+            msc.scanFile(Environment.getExternalStorageDirectory().getPath()+"image.jpg", "image/jpeg");
+        }
+
+        @Override
+        public void onScanCompleted(String s, Uri uri) {
+            msc.disconnect();
+        }
+
+    });
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
